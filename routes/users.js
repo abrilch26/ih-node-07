0685edabbf780b19de8cd402c12273c16394054d
“Abril <abril.ch.26@gmail.com>
2021-11-24 11:20:56 -0600

    basic structure

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+const express		= require("express")
+const router		= express.Router()
+
+const usersController	= require("./../controllers/usersControllers")
+
+
+router.get("/", usersController.register)
+
+module.exports = router
